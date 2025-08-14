6d729c6f1e03e2ac61c48742a3dedbb683db08cb
Saptaparni Kumar <saptaparnikumar@Saptaparnis-MacBook-Air-2.local>
2025-08-14 14:13:01 +0300

    Commit for adding Procfile (makefile) to assist deployment on Railway

--- app.py
+++ app.py
@@ -80,5 +80,10 @@ def chat():
         print("Error:", e)
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
